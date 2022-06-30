319ba1c1c708a180f2567605fdcea82a56ea3cf2
Agnes76 <ugochukwuagnes20@gmail.com>
2022-06-30 09:59:07 +0100

    first commit

---
+++ LearningAlgo/PlusMinus.cs
@@ -0,0 +1,126 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+namespace LearningAlgo
+{
+    public class PlusMinus
+    {
+        //        Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero.Print the decimal value of each fraction on a new line with  places after the decimal.
+
+        //Note: This challenge introduces precision problems.The test cases are scaled to six decimal places, though answers with absolute error of up to are acceptable.
+
+        //Example
+        // arr = {1, 1, 0, -1, -1};
+        //There are n = 5 elements, two positive, two negative and one zero.Their ratios are 2/5 = 0.400000,2/5 = 0.400000 and 1/5 = 0.200000.Results are printed as:
+
+        //0.400000
+        //0.400000
+        //0.200000
+        //Function Description
+
+        //Complete the plusMinus function in the editor below.
+
+        //plusMinus has the following parameter(s) :
+
+        // *int arr[n]: an array of integers
+        //Print
+        //Print the ratios of positive, negative and zero values in the array.Each value should be printed on a separate line with digits after the decimal. The function should not return a value.
+
+        //Input Format
+
+        //The first line contains an integer, , the size of the array.
+        //The second line contains  space-separated integers that describe .
+
+        //Constraints
+        // 0 < n <= 100
+        // -100 < arr[i] <= 100
+
+
+        //Output Format
+
+        //Print the following  lines, each to  decimals:
+
+        //1. proportion of positive values
+        //2. proportion of negative values
+        //3. proportion of zeros
+        //Sample Input
+
+        //STDIN           Function
+        //-----           --------
+        //6               arr[] size n = 6
+        //- 4 3 -9 0 4 1   arr = [-4, 3, -9, 0, 4, 1]
+        //Sample Output
+
+        //0.500000
+        //0.333333
+        //0.166667
+        //Explanation
+
+        //There are positive numbers, negative numbers, and zero in the array.
+        //The proportions of occurrence are positive:3/6=0.500000 , negative:2/6=0.333333  and zeros:1/6=0.166667 .
+
+        public static void plusMinus(List<int> arr)
+        {
+            double positive = 0;
+            double negative = 0;
+            double zero = 0;
+            double total = arr.Count;
+
+            // for(int i = 0; i < arr.Count; i++)
+            // {
+            //    if(arr[i] > 0) {
+            //        positive++;
+            //    }
+            //    if(arr[i] < 0){
+            //        negative++;
+            //    }
+            //    if(arr[i] == 0){
+            //        zero++;
+            //    }
+            // }
+
+            foreach (var element in arr)
+            {
+                if (element > 0)
+                {
+                    positive++;
+                }
+                if (element < 0)
+                {
+                    negative++;
+                }
+                if (element == 0)
+                {
+                    zero++;
+                }
+            }
+
+            Console.WriteLine(string.Format("{0:N6}", positive / total));
+            Console.WriteLine(string.Format("{0:N6}", negative / total));
+            Console.WriteLine(string.Format("{0:N6}", zero / total));
+
+        }
+         static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            PlusMinus.plusMinus(arr);
+        }
+    }
+        
+}
+
---
+++ LearningAlgo/staircase.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningAlgo
+{
+    public class staircase
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static void Print(int n)
+        {
+            
+            for (int i = 1; i <= n; i++)
+            {
+                string str = "";
+                for (int j = 0; j < i; j++)
+                {
+                    str += "#";
+                }
+                Console.WriteLine(str);
+            }
+           
+        }
+        // Explanation
+        //1. when i = 1, it iterate through the first loop and declare an empty string
+        // Then iterates the second loop once and concatenate "#", go through the second 
+        // loop again, j = 1, and i is 1 which makes it break from the second loop
+
+        //2.  when i = 2, it iterate through the first loop and declare an empty string
+        // Then iterates the second loop once and concatenate "#", go through the second 
+        // loop again, j = 1, and i is 2, it will concatenate "#" again, Then go through the same loop
+        // j= 2 now and i = 2, so it will break (making ##)
+
+        //3. when i = 3, it iterate through the first loop and declare an empty string
+        // Then iterates the second loop once and concatenate "#", go through the second 
+        // loop again, j = 1, and i is 2, it will concatenate "#" again, Then go through the same loop
+        // j= 2 now and i = 3,it will concatenate "#" again, go through the loop again, j=3 and i =3,
+        // so it will break (making ###)
+
+        public static void PrintDetails(int n)
+        {
+
+            for (int i = n; i >= 1; i--)
+            {
+                string str = "";
+                for (int j = i; j > 1; j--)
+                {
+                    str += " ";
+                }
+                for (int k = n; k >= i; k--)
+                {
+                    str += "#";
+                }
+                Console.WriteLine(str);
+            }
+
+        }
+
+        //explanation
+        //1. when it iterate through the first loop, let's assume n = 5, i would be equal to n which is 5
+        // the 5 is greater than 1, it would declare the empty string, then enters the first inner loop,check
+        // the condition, j = i which is 5, 5 is greater than 1, it would enter, declare the whitespace(" "), and
+        // decrement j to 4, 4 is greater than 1, it would enter again, declare a second whitespace(" ") and decrement
+        // j to 3, 3 is still greater than 1, it would enter again, declare a third whitespace(" ") and decrement j to 2,
+        // 2 is still greater than 1, it would enter again , declare the fourth whitespace(" ") and decrement j to 1,
+        // 1 is not greater than 1, it would break out of that loop and enter the Second inner loop, check the
+        // condition K = n which is 5(had to use n cos it is a constant), K which is 5 is greater than or equal to i
+        // which is 5, it would enter the loop and concatenate ("#") to the first 4 empty spaces, decrement k to 4, 4 is
+        // not greater than or equal to i which is 5, so it breaks out of that loop. And start from the outer loop
+
+        //2. it iterate through the first loop, i is now equal to 4, 4 is greater than 1, it would declare the empty string,
+        //then enters the first inner loop,check the condition, j = i which is now 4, 4 is greater than 1, it would enter,
+        //declare the whitespace(" "), decrement j to 3, 3 is still greater than 1, it would enter again, declare a second
+        //whitespace(" ") and decrement j to 2, 2 is still greater than 1, it would enter again , declare the third whitespace(" ")
+        //and decrement j to 1, 1 is not greater than 1, it would break out of that loop and enter the Second inner loop, check the
+        // condition K = n which is 5(had to use n cos it is a constant), K which is 5 is greater than i
+        // which is 4, it would enter the loop and concatenate ("#") to the first 4 empty spaces, decrement k to 4, 4 is
+        // not greater than or equal to i which is 5, so it breaks out of that loop. And start from the outer loop
+
+    }
+
+}
